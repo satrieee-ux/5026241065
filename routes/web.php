2a9e06bcdca03024e576dc3d35b26b4619013f12
satrieee-ux <satrieee@gmail.com>
2026-05-02 15:19:31 +0700

    Pertemuan 8 V1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 Route::get('/', function () {
     return view('welcome');
@@ -52,6 +54,12 @@ Route::get('perlimaan', function () {
 });
 
 Route::get('dosen',[DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
 
-
-  Route::get('dosen',[DosenController::class, 'index']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
