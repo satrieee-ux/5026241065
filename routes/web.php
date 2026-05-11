86e9fdc2bca132c5aeb134ac2e09d878bccd65f3
satrieee-ux <satrieee@gmail.com>
2026-05-11 11:45:03 +0700

    Pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController ;
 use App\Http\Controllers\BlogController ;
+use App\Http\Controllers\PegawaiDBController ;
 
 Route::get('/', function () {
     return view('welcome');
@@ -56,10 +57,12 @@ Route::get('perlimaan', function () {
 Route::get('dosen',[DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/pegawainama/{nama}', [PegawaiController::class, 'index']); //janlup ganti
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 //blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+//crud tabel pegawai
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
